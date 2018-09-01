455db4108d4a5777735f7a4fae959f0bd2e14477
german valdes <germanvs93@hotmail.com>
2018-09-01 18:21:49 +1200

    Service worker check in app load whether there is a new version available and prompts user to reload page if so

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {TodoService} from './services/todo.service';
 import {Todo} from './model/todo.model';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +12,19 @@ export class AppComponent implements OnInit {
   title = 'PWA';
   todoList: Todo[] = [];
 
-  constructor(private todoService: TodoService) {
+  constructor(private todoService: TodoService,
+              private swUpdate: SwUpdate) {
   }
 
   ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('New version available, load new Vesion?')) {
+          window.location.reload();
+        }
+      });
+    }
+
     this.todoService.todoList().subscribe((data: Todo[]) => {
       this.todoList = data;
     });
