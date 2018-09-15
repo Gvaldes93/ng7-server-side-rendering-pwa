2f576bf21e664cc7c0f577028dd46f0af8b71c94
german valdes <germanvs93@hotmail.com>
2018-09-15 12:16:40 +1200

    Updated db data and removed checklist of anuglar app

--- server/database-data.ts
+++ server/database-data.ts
@@ -3,17 +3,17 @@
 module.exports = {
   1: {
     'id': 1,
-    'title': 'Read something ',
+    'title': 'Housing',
     'complete': false
   },
   2: {
     'id': 2,
-    'title': 'Clean inbox',
+    'title': 'Sports',
     'complete': false
   },
   3: {
     'id': 3,
-    'title': 'Make restaurant reservation',
+    'title': 'Locals',
     'complete': false
   }
 };
